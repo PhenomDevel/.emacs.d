c32ce46d7b964cb335865d0fa74c07a49b70e815
Kevin Kaiser <13941819+PhenomDevel@users.noreply.github.com>
2022-05-17 15:13:54 +0200

    `my-git-grep` von Falko hinzugefügt

--- config/setup__counsel.el
+++ config/setup__counsel.el
@@ -1,18 +1,29 @@
 ;; Enhanced emacs core functions (mostly used for counsel-git-grep (:)
 
+(defun my-region-text ()
+  (if mark-active
+      (buffer-substring-no-properties (region-beginning) (region-end))))
+
+(defun my-git-grep ()
+  (interactive)
+  (let ((text (my-region-text)))
+    (deactivate-mark)
+    (counsel-git-grep text)))
+
 (use-package
- counsel
+  counsel
 
- :ensure t
+  :ensure t
 
- :commands
- (counsel-git-grep)
+  :commands
+  (counsel-git-grep)
 
- ;; TODO: Keybindings
- :bind
- (("C-c gg" . counsel-git-grep))
+  ;; TODO: Keybindings
+  :bind
+  (("C-c gg" . counsel-git-grep)
+   ("C-c gs" . my-git-grep))
 
- ;; Add more
- )
+  ;; Add more
+  )
 
 (provide 'setup__counsel)
